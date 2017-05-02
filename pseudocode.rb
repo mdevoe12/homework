67a5f95ec239a59fa25255872b43b6466c97a0b7
Matt DeVoe <matthew.devoe@gmail.com>
2017-05-02 09:24:06 -0600

    name and date

---
+++ pseudocode.rb
@@ -0,0 +1,7 @@
+Matt DeVoe 5/2/17
+
+#If the input from user is nil, repeat Hello
+#If the input from user is not nil and lower case, output I AM HAVING A HARD TIME HEARING YOU
+#If the input from user is not nil and upper case, output NO, THIS IS NOT A PET STORE
+#If GOODBYE! is written, a counter needs to increase by 1
+#When the counter for GOODBYE! reaches 2, the output will read THANK YOU FOR CALLING! the program will then exit
